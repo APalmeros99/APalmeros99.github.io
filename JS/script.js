51ea9a8b673c2174cd8a3a96938b79890a3e3a40
APalmeros99 <58722591+APalmeros99@users.noreply.github.com>
2021-10-19 00:43:06 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,83 +1,86 @@
 /*
+    This JS file makes a Graph data structure with nodes and edges as an adjecency list and then finds the shortest path
+    from a specifed set of starting and destination nodes using a Breadth First Search algorithm.
+
     function Graph()
-        this function makes a Graph data structure that simple connects nodes to other nodes. the connection
+        this function makes a Graph data structure that simply connects nodes to other nodes. the connection
         is an edge.
     
     function shortestPath()
-        this function uses Breadth First Search to find all paths
-
+        this function uses Breadth First Search to find all existing paths. While keeping track of previous vertex
+        this allows us to get the shortest path from destination to the starting node, which we can reverse to
+        make the list print from the starting node to the destination.
 */
 function Graph()
 {
-    var neighbors = this.neighbors = []; // Key = vertex, value = array of neighbors.
+    var neighbors = this.neighbors = [];    // Key = vertex, value = array of neighbors.
   
-    this.addEdge = function (u, v)
+    this.addEdge = function (x, y)          
     {      
-        if (neighbors[u] === undefined)
-        {                                  // Add the edge u -> v. with strict comparison of ===
-            neighbors[u] = [];
+        if (neighbors[x] === undefined)     // If inputs are empty, they are ignored.
+        {                                   // Add the edge x -> y.
+            neighbors[x] = [];
         }
-        neighbors[u].push(v);
-        if (neighbors[v] === undefined)
-        {                                  // Also add the edge v -> u in order                        
-            neighbors[v] = [];               // to implement an undirected graph.
-        }                                  // For a directed graph, delete
-        neighbors[v].push(u);              // these four lines.
+        neighbors[x].push(y);
+        if (neighbors[y] === undefined)
+        {                                   // Also add the edge y -> x in order                        
+            neighbors[y] = [];              // to implement an undirected graph.
+        }                                   
+        neighbors[y].push(x);               
     };
     return this;
 }
  
 function shortestPath(graph, start, destination)
 {
-    if (start == destination)               //finishes function if destination is same as starting point
-    {                                       // EX: start = USA, destination = USA
+    if (start == destination)               // Finishes function if destination is same as starting point.
+    {                                       // EX: start = USA, destination = USA.
         print(start);          
         return;                 
     }                           
-    var queue = [ start ]
-    var visited = { start: true };
-    var predecessor = [];
-    var tail = 0;
+    var queue = [start]                     // Queue array with the variable 'start' in the first index.
+    var visited = {start: true};            // Visited object with booleans to track visited nodes.
+    var predecessor = [];                   // Array to keep track of previous vertex.
 
-    while (tail < queue.length)
+    while (queue.length > 0)
     {
-        var u = queue[tail++];              // Pop a vertex off the queue.
-        var neighbors = graph.neighbors[u];
+        var x = queue.shift();              // Pop first vertex off the queue.
+        var neighbors = graph.neighbors[x]; // Gets nodes connected to the initial node as neighbor nodes. (Vertex)
         //console.log(neighbors);
         for (var i = 0; i < neighbors.length; ++i) 
         {
-            var v = neighbors[i];
-            if (visited[v])
+            var y = neighbors[i];           // Check if the current set of neighbor nodes has been visited
+            if (visited[y])                 // If it has, go on to the next check.
             {
                 continue;
             }
-            visited[v] = true;
-            if (v === destination)
-            {                       // Check if the path is complete.
-                var path = [ v ];   // If so, backtrack through the path.
-                while (u !== start)
+            visited[y] = true;              // Update the status of the set of neighbors to visited=true.
+            if (y === destination)          // Check if the path is complete.
+            {                               // If so, backtrack through the path until it hits the starting node.
+                var path = [y];   
+                while (x !== start)
                 {
-                    path.push(u);
-                    u = predecessor[u];
+                    path.push(x);           // Add the set of nodes to the path array.
+                    x = predecessor[x];     
                 }
-                path.push(u);
-                path.reverse();
-                print(path.join(' --> '));
+                path.push(x);               // Makes final path array. 
+                path.reverse();             // Since we backtracked through the route, we need to reverse
+                print(path.join(' --> '));  // the list so it starts from the starting node instead of the destination.
                 //console.log(path);
                 return;
             }
-            predecessor[v] = u;
-            queue.push(v);
+            predecessor[y] = x;
+            queue.push(y);
             //console.log(predecessor);
         }
     }
 }
   
-  function print(output)
+  function print(output)                    // Function to print output with a line break
   {
         document.getElementById('display').innerHTML += output + '<br>';
   }
-    function fun() {
+    function findRoute() {                        // Function that initializes a Graph and adds nodes and edges to it.
     var graph = new Graph();
     graph.addEdge('CAN', 'USA');
     graph.addEdge('USA', 'MEX');
@@ -89,8 +92,6 @@ function shortestPath(graph, start, destination)
     graph.addEdge('NIC', 'CRI');
     graph.addEdge('CRI', 'PAN');
     
-    var dest = document.getElementById('fname').value;
-    shortestPath(graph, 'USA', dest);
-  };
-
- 
+    var dest = document.getElementById('fname').value;    // Gets user input from the form in index.html
+    shortestPath(graph, 'USA', dest);                     // calls function with starting node of 'USA' and dynamic destination-
+  };                                                      // depending on user input.
